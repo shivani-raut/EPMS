ab581b40b16168bba2a068fbbe8df37892cb4e94
shivani-raut <78538342+shivani-raut@users.noreply.github.com>
2022-09-06 16:25:21 +0530

    ShivaniRaut

---
+++ Bank_detailsDML.sql
@@ -0,0 +1,8 @@
+insert into bank_details values ('A00001','HDFC BANK','HDFC0034',101);
+insert into bank_details values ('A00003','HDFC BANK','HDFC0021',102);
+insert into bank_details values ('A00006','AXIS BANK','AXIS0023',103);
+insert into bank_details values ('A00034','AXIS BANK','AXIS0045',104);
+insert into bank_details values ('A00023','YES BANK','YES00387',105);
+insert into bank_details values ('A00067','YES BANK','YES00345',106);
+insert into bank_details values ('A00045','YES BANK','YES00567',107);
+insert into bank_details values ('A00065','AXIS BANK','AXIS0090',108);
