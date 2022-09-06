7d2eef08eec09428b5d2c8bd3bfb3a5de839d539
shivani-raut <78538342+shivani-raut@users.noreply.github.com>
2022-09-06 15:42:05 +0530

    ShivaniRaut

---
+++ BandDML.sql
@@ -0,0 +1,6 @@
+insert into band values (1,'Band X TR',21000,1800,1000);
+insert into band values (2,'Band 1 ASS',36000,3700,5000);
+insert into band values (3,'Band 2 BD_SR',43000,5160,10000);
+insert into band values (4,'Band 3 BD_MGR',56000,6720,12000);
+insert into band values (5,'Band 4 BD_CH',70000,8400,15000);
+insert into band values (6,'Band 5 BD_VP',75000,9000,18000);
