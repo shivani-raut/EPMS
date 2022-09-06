0272c2b606a0cb05e7a323bcb5189d807814bdb2
awasthi000 <112246860+awasthi000@users.noreply.github.com>
2022-09-06 16:20:30 +0530

    VARUN

---
+++ allowanceDML.sql
@@ -0,0 +1,10 @@
+---allowance
+
+
+
+insert into allowances values ('101','medical','partial_taxable');
+insert into allowances values ('102','house_rent','partial_taxable');
+insert into allowances values ('103','overtime','taxable');
+insert into allowances values ('104','special_allowance','partial_taxable');
+insert into allowances values ('105','meal','partial_taxable');
+insert into allowances values ('106','transport','partial_taxable');
